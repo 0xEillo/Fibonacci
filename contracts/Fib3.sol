0efb3b3afa78b581d2b2799eee1bd8691ee507e1
Oliver Ryall <67156692+Oliver-Ryall@users.noreply.github.com>
2022-04-06 16:36:19 +0100

    Update Fib3.sol

--- contracts/Fib3.sol
+++ contracts/Fib3.sol
@@ -1,10 +1,12 @@
 pragma solidity 0.8.13;
 
+// The contents stored in the stack are erased after every function call
+
 contract Fib3 {
     fallback(bytes calldata _input) external payable returns(bytes memory _output) {
-        uint256 N = msg.value;
-        uint256 a = 1;
-        uint256 b = 1;
+        uint256 N = msg.value;  // N representing the inputed uint is stored in the stack
+        uint256 a = 1;  // a is stored in the stack
+        uint256 b = 1; // b is stored in the stack
 
         if (N == 0) {
             uint256 number = 0;
@@ -12,7 +14,7 @@ contract Fib3 {
         }
 
         for (uint256 i = 2; i < N; i++) {
-            uint256 c = a + b;
+            uint256 c = a + b;  // c is stored in the stack
             a = b;
             b = c;
         }
