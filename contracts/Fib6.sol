9d7befcc54004d28ac4621511f10ae8cd5dc8c1b
Oliver Ryall <67156692+Oliver-Ryall@users.noreply.github.com>
2022-04-07 09:46:41 +0100

    Update Fib6.sol

--- contracts/Fib6.sol
+++ contracts/Fib6.sol
@@ -1,24 +1,22 @@
 pragma solidity 0.8.13;
 
-// The contents stored in the stack are erased after every function call
-
 library Fib {
     function run(uint256 N) public pure returns (bytes memory) {
-        uint256 a = 1; // a is stored in the stack
-        uint256 b = 1; // b is stored in the stack
+        uint256 a = 1;
+        uint256 b = 1; 
 
         if (N == 0) {
             uint256 number = 0;
-            return abi.encodePacked(number); // returning the uint as bytes
+            return abi.encodePacked(number); 
         }
 
         for (uint256 i = 2; i < N; i++) {
-            uint256 c = a + b; // c is stored in the stack
+            uint256 c = a + b; 
             a = b;
             b = c;
         }
 
-        return abi.encodePacked(b); // returning the uint as bytes
+        return abi.encodePacked(b); 
     }
 }
 
